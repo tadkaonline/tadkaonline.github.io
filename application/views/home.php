bc3b4f869659121f5d2a5e5ed5c6bc1bac4dcbdf
Shivam Dixit <shivamd001@gmail.com>
2014-04-20 09:47:25 +0530

    Initial commit

---
+++ application/views/home.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>TADKA Online</title>
+		<meta name="author" content="shivam">
+		<link rel="stylesheet" type="text/css" href="">
+	</head>
+
+	<body>
+		<h1>Hello World !</h1>
+	</body>
+</html>
