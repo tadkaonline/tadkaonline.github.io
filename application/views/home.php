4d8cbee880143c2c5fab1c7670440b99e0b3d059
Shivam Dixit <shivamd001@gmail.com>
2014-04-20 11:01:17 +0530

    Created home page template

--- application/views/home.php
+++ application/views/home.php
@@ -2,10 +2,129 @@
 	<head>
 		<title>TADKA Online</title>
 		<meta name="author" content="shivam">
-		<link rel="stylesheet" type="text/css" href="">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="<?php echo site_url('/media/css/bootstrap.css')?>">
+		<link rel="stylesheet" type="text/css" href="<?php echo site_url('/media/css/style.css')?>">
 	</head>
 
 	<body>
-		<h1>Hello World !</h1>
+
+		<!--le navbar
+		===============-->
+		<div class="navbar navbar-inverse navbar-static-top">
+			<div class="container">
+
+				<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+
+				<a href="#" class="navbar-brand">TADKA</a>
+
+				<div class="collapse navbar-collapse navHeaderCollapse">
+					<ul class="nav navbar-nav navbar-left">
+						<li><a href="#">Add Item</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<div class="page-header text-center">
+								<h3 class="text-danger sender-name">Tarun Gupta</h3>
+								<span class="glyphicon glyphicon-time text-left"></span>
+								<span class="">14 mintues remaining</span>
+							</div>
+
+							<div class="page-body">
+								<table class="table table-striped">
+									<tbody>
+										<tr>
+											<th>Time</th>
+											<td>12:45 pm</td>
+										</tr>
+										<tr>
+											<th>Room No</th>
+											<td>C-002</td>
+										</tr>
+										<tr>
+											<th>Hostel</th>
+											<td>BH-2</td>
+										</tr>
+										<tr>
+											<th>Phone</th>
+											<td>+91-123-456-789</td>
+										</tr>
+										<tr>
+											<th>Comments</th>
+											<td>Any Comment</td>
+										</tr>
+									</tbody>
+								</table>
+								<div class="text-center">
+									<a href="#" class="btn btn-info btn-sm">View Rate List</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<div class="page-header text-center">
+								<h3 class="text-danger sender-name">Sai Siddharth</h3>
+								<span class="glyphicon glyphicon-time text-left"></span>
+								<span class="">25 mintues remaining</span>
+							</div>
+
+							<div class="page-body">
+								<table class="table table-striped">
+									<tbody>
+										<tr>
+											<th>Time</th>
+											<td>12:55 pm</td>
+										</tr>
+										<tr>
+											<th>Room No</th>
+											<td>C-007</td>
+										</tr>
+										<tr>
+											<th>Hostel</th>
+											<td>BH-2</td>
+										</tr>
+										<tr>
+											<th>Phone</th>
+											<td>+91-123-456-789</td>
+										</tr>
+										<tr>
+											<th>Comments</th>
+											<td>Any Comment</td>
+										</tr>
+									</tbody>
+								</table>
+								<div class="text-center">
+									<a href="#" class="btn btn-info btn-sm">View Rate List</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+		</div>
+
+		<!--le scripts
+		===============-->
+		<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script type="text/javascript" src="<?php echo site_url('/media/js/bootstrap.js')?>"></script>
 	</body>
 </html>
